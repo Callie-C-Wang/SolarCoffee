6bb7232f0f94a3a72fd0ac9a0b6469f336d8b6c0
Cong Wang <callie.c.wang@gmail.com>
2020-11-10 22:09:19 -0500

    add C# objects for mapping table in database

---
+++ SolarCoffee.Data/SolarDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SolarCoffee.Data.Models;
+
+namespace SolarCoffee.Data
+{
+    public class SolarDbContext : IdentityDbContext {
+        public SolarDbContext() { } // default constructor
+
+        public SolarDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public virtual DbSet<Customer> Customers {get; set; } // developer needs to create C# class with name Customer
+        public virtual DbSet<CustomerAddress>
+    }
+}
